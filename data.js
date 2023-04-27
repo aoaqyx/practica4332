8cb1ea4e4127440ef14c3f32dd9ad1637b6816d3
aoaqyx <126892744+aoaqyx@users.noreply.github.com>
2023-04-27 18:03:27 +0300

    Add files via upload

---
+++ data.js
@@ -0,0 +1,21 @@
+// data.js
+export const context = {
+  languages: [
+    {
+      name: 'HTML',
+      modern: true,
+    },
+    {
+      name: 'CSS',
+      modern: true,
+    },
+    {
+      name: 'JavaScript',
+      modern: true,
+    },
+    {
+      name: 'COBOL',
+      modern: false,
+    },
+  ],
+};
